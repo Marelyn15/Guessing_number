05ccc3c1352299a3c2e783bac162929afd4d47a5
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-09-25 19:23:15 -0700

    Initial commit

---
+++ guessing_number.py
@@ -0,0 +1,46 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+random_number = random.randint(1,100)
+if difficulty == 'easy':
+    # Attemps
+    attemps = 10
+    while attemps > 0:
+        print(f"You have {attemps} remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess == random_number:
+            print(f"Very well. You guessed the number {guess}")
+            break
+        elif guess < random_number:
+            print("Too low")
+            attemps = attemps - 1
+        elif guess > random_number:
+            print("Too high")
+            attemps = attemps -1
+        else:
+            print("You put a wrong input, try again.")
+elif difficulty == 'hard':
+    # Attemps
+    attemps = 5
+    while attemps > 0:
+        print(f"You have {attemps} remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess == random_number:
+            print(f"Very well. You guessed the number {guess}")
+            break
+        elif guess < random_number:
+            print("Too low")
+            attemps = attemps - 1
+        elif guess > random_number:
+            print("Too high")
+            attemps = attemps -1
+        else:
+            print("You put a wrong input, try again.")
+else:
+    print("You chose a wrong world try again.")
+
+
+
